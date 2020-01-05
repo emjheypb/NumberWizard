26e2ffaaf274ea5b869cc374fdfadd66071f72c5
Mariah Baysic <mjpb123@gmail.com>
2020-01-05 21:14:48 +0800

    initial github commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min, max;
+    int guess;
+    [SerializeField] Text guessDisplay;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        UpdateGuess();
+        max += 1;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        UpdateGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        UpdateGuess();
+    }
+
+    void UpdateGuess()
+    {
+        guess = Random.Range(min, max);
+        guessDisplay.text = (guess.ToString());
+    }
+}
